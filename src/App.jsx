c0f53c4ee86f690d8713058c75528be3540fadde
Anoop Singh <anoop@Anoops-MacBook-Pro-2.local>
2026-06-15 13:57:06 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id="center" className="text-3xl font-bold underline">
+        hello world 
+       </div>
+    </>
+  )
+}
+
+export default App
