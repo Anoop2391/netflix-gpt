9367cb8c46a2a372d872f1e842ea82e12b766134
Anoop Singh <anoop@Anoops-MacBook-Pro-2.local>
2026-06-25 22:28:20 +0530

    fix search bg on big screens.

--- src/components/GptSearch.jsx
+++ src/components/GptSearch.jsx
@@ -5,8 +5,12 @@ import GptSearchBar from "./GptSearchBar";
 const GptSearch = () => {
   return (
     <div className="flex flex-col items-center relative">
-      <div className="absolute w-screen h-screen -z-10">
-        <img src={BACKGROUND_IMAGE} alt="background" />
+      <div className="fixed inset-0 -z-10">
+        <img
+          src={BACKGROUND_IMAGE}
+          alt="background"
+          className="w-full h-full object-cover"
+        />
       </div>
       <GptSearchBar />
       <GptMovieSuggestions />
